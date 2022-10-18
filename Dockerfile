f600d4c95c65a597bd4f4a43e5467c204f24c432
SE-Yukun <yukun@anylinks.jp>
2022-10-19 03:33:03 +0900

    update

--- Dockerfile
+++ Dockerfile
@@ -2,37 +2,44 @@ FROM archlinux:latest
 
 ENV MEM=1G
 ENV RCONPASSWD=minecraft
-ENV STATICVER=void
+ENV VERSION=void
 
 WORKDIR /root
-RUN pacman -Syyu jre-openjdk-headless jq --noconfirm
-RUN curl -sLo version https://api.purpurmc.org/v2/purpur
-RUN curl -sLo minecraft.jar https://api.purpurmc.org/v2/purpur/$(cat version | jq -r .versions[-1])/latest/download
 RUN mkdir /root/minecraft
-RUN mv /root/minecraft.jar /root/minecraft
+RUN mkdir /root/require-files
+RUN mkdir /tmp/minecraft
+RUN mkdir /tmp/rcon
+RUN pacman -Syyu jre-openjdk-headless jq bc --noconfirm
 
-WORKDIR /root/minecraft
-RUN echo "eula=true" > eula.txt
-RUN bash -c "java --add-modules=jdk.incubator.vector -Xmx$MEM -Xms$MEM -jar minecraft.jar nogui & sleep 40 && killall -9 java"
-RUN rm -rf world
-RUN echo enable-rcon=true >> server.properties
-RUN echo rcon.password=$RCONPASSWD >> server.properties
-RUN echo rcon.port=25575 >> server.properties
+WORKDIR /tmp/minecraft
+RUN curl -sLo minecraft.jar https://api.purpurmc.org/v2/purpur/$(curl -sL https://api.purpurmc.org/v2/purpur | jq -r .versions[-1])/latest/download && \
+    echo "eula=true" > eula.txt && \
+    java --add-modules=jdk.incubator.vector -Xmx$MEM -Xms$MEM -jar ./minecraft.jar nogui & \
+    sleep 15 && \
+    killall -9 java && \
+    echo enable-rcon=true >> server.properties && \
+    echo rcon.password=$RCONPASSWD >> server.properties && \
+    echo rcon.port=25575 >> server.properties && \
+    cp server.properties /root/require-files && \
+    cp eula.txt /root/require-files && \
+    rm ./*
 
 # rcon install
+WORKDIR /tmp/rcon
 RUN pacman -S pkgconf make gcc cmake check libbsd git --noconfirm && git clone https://github.com/n0la/rcon.git && \
-    cd /root/minecraft/rcon && \
-    mkdir build && \
-    cd /root/minecraft/rcon/build && \
-    cmake .. -DCMAKE_INSTALL_PREFIX=/usr && \
-    make && \
-    make install && \
-    cd /root/minecraft && \
-    pacman -Rs pkgconf make gcc cmake check libbsd git --noconfirm
+    mkdir build && cd ./build && \
+        cmake .. -DCMAKE_INSTALL_PREFIX=/usr && \
+        make && \
+        make install && \
+    cd ../ && \
+    pacman -Rs pkgconf make gcc cmake check libbsd git --noconfirm && \
+    rm ./*
 
 COPY ./main.sh /root/
 COPY ./buckup.sh /root/
+COPY ./alart.sh /root/
 
-RUN chmod +x /root/main.sh /root/buckup.sh 
+WORKDIR /root
+RUN chmod +x ./main.sh
 
-CMD ["bash", "-c", "/root/main.sh"]
+CMD ["bash", "-c", "./main.sh"]
--- main.sh
+++ main.sh
@@ -1,33 +1,44 @@
 #!/bin/bash
 
-cd /root/minecraft
-INSTALLVER=void
-if [ $STATICVER != "void" ]; then
-  echo "Checking Version ($(echo $STATICVER))"
-  if [ ! -e version.txt ]; then
-    touch version.txt
+cd ./minecraft
+
+if [ ! -e eula.txt ]; then
+  cp /root/require-files/* ./
+fi
+
+if [ ! -e version.txt ]; then
+  if [ $VERSION = "void" ]; then
+    echo $(curl -sL https://api.purpurmc.org/v2/purpur | jq -r ".versions[-1]") >version.txt
+    curl -sLo minecraft.jar https://api.purpurmc.org/v2/purpur/$(cat version.txt)/latest/download
   fi
-  if [ $(cat version.txt) != $STATICVER ]; then
-    if [ $(curl -sL https://api.purpurmc.org/v2/purpur | jq -r ".versions[]|select(index(\"$STATICVER\"))" | awk 'END{print}') != "" ]; then
-      rm version.txt
-      echo $(curl -sL https://api.purpurmc.org/v2/purpur | jq -r ".versions[]|select(index(\"$STATICVER\"))" | awk 'END{print}') > version.txt
+  if [ $VERSION != "void" ]; then
+    if [ $(curl -sL https://api.purpurmc.org/v2/purpur | jq -r ".versions[]|select(index(\"$VERSION\"))" | awk 'END{print}') != "" ]; then
+      echo $(curl -sL https://api.purpurmc.org/v2/purpur | jq -r ".versions[]|select(index(\"$STATICVER\"))" | awk 'END{print}') >version.txt
+      curl -sLo minecraft.jar https://api.purpurmc.org/v2/purpur/$(cat version.txt)/latest/download
     fi
-INSTALLVER=$(cat version.txt)
-  fi 
-fi
-if [ $STATICVER = "void" ]; then
-  if [ ! -e version.txt ]; then
-    touch version.txt
   fi
-  if [ $(cat version.txt) !=  $(curl -sL https://api.purpurmc.org/v2/purpur | jq -r ".versions[-1]") ]; then
+fi
+
+if [ $VERSION = void ]; then
+  if [ $(cat version.txt) <$(curl -sL https://api.purpurmc.org/v2/purpur | jq -r ".versions[-1]" | awk -F. '{printf "%2d%02d%02d", $1,$2,$3}')]; then
     rm version.txt
-    echo $(curl -sL https://api.purpurmc.org/v2/purpur | jq -r ".versions[-1]") > version.txt
-INSTALLVER=$(cat version.txt)
+    echo $(curl -sL https://api.purpurmc.org/v2/purpur | jq -r ".versions[-1]") >version.txt
+    curl -sLo minecraft.jar https://api.purpurmc.org/v2/purpur/$(cat version.txt)/latest/download
   fi
 fi
-if [ $INSTALLVER != "void" ]; then 
-  rm minecraft.jar
-  curl -sLo minecraft.jar https://api.purpurmc.org/v2/purpur/$(echo $INSTALLVER)/latest/download
+
+if [ $VERSION != void ]; then
+  if [ $(echo $VERSION | awk -F. '{printf "%2d%02d%02d", $1,$2,$3}') ] <$(curl -sL https://api.purpurmc.org/v2/purpur | jq -r ".versions[-1]" | awk -F. '{printf "%2d%02d%02d", $1,$2,$3}'); then
+    rm version.txt
+    echo $(curl -sL https://api.purpurmc.org/v2/purpur | jq -r ".versions[-1]") >version.txt
+    curl -sLo minecraft.jar https://api.purpurmc.org/v2/purpur/$(cat version.txt)/latest/download
+  fi
 fi
+
+chmod +x /root/buckup.sh
+chmod +x /root/alart.sh
+
 /root/buckup.sh &
+/root/alart.sh &
+
 java --add-modules=jdk.incubator.vector -Xmx$MEM -Xms$MEM -jar minecraft.jar nogui
